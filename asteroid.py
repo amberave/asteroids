8d2fc9e944ec36378bbd8f6b2bd0aabb28234749
amberave <kwynter013@gmail.com>
2025-07-12 11:58:53 +1000

    completed!

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+import random
+from constants import *
 
 class Asteroid(CircleShape):
 
@@ -11,5 +13,17 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        vector1 = self.velocity.rotate(angle)
+        vector2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
 
-
+        asteroid1 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid2 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid1.velocity = vector1 * 1.2
+        asteroid2.velocity = vector2 * 1.2
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from circleshape import CircleShape
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     print("Starting Asteroids!")
@@ -17,15 +19,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
 
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Player.containers = (updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
 
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
@@ -33,6 +39,15 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        
+        for o in asteroids:
+            if o.is_collision(player):
+                print("Game over!")
+                sys.exit()
+            for s in shots:
+                if o.is_collision(s):
+                    o.split()
+                    s.kill()
         for d in drawable:
             d.draw(screen)
         pygame.display.flip()
